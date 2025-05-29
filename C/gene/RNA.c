aa268177fb82ac70346b01c2ebedee27a70c0c14
login in Steam <155938693+fcrownarisk@users.noreply.github.com>
2025-05-29 10:11:32 +0800

    Update RNA.c

--- C/gene/RNA.c
+++ C/gene/RNA.c
@@ -1,9 +1,9 @@
 void hexagon(){
-char hexagon =
-    "/-\
+char pentagon =
+    "/ \
      \_/";
 }
-void RNA(hexagon){
+void RNA(pentagon){
    ["A : a / b '\' c '\' d _ e /"]
    ["C : f / g '\' i '\' j _ k /"]
    ["G : l / m '\' n '\' o _ p /"]
