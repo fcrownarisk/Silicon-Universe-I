7ed2d8ef03308d9bd5a3dca54122be9569b1821b
String Literal <155938693+fcrownarisk@users.noreply.github.com>
2025-08-21 04:41:52 +0800

    Create gene.c

---
+++ C/gene/gene.c
@@ -0,0 +1,56 @@
+
+void DNA(){
+   char Thymine =
+        "O"  
+        "||"
+   "CH2\/  |\NH"
+      "|   |\\O"
+       "\NH/"   ;
+    char Cytosine =
+       "NH2"
+        "|"
+       "/ \N"
+       "||  |"
+       "|| \\O"
+       "\NH/"   ;
+    char Adenine =
+       "//N\/NH2\\N"
+       "/  ||     |"
+       "\  ||     |"
+       "\NH/\ N  //" ;
+    char Guanine =
+             "O"
+            "| |"  
+       "//N\/NH2\\N"
+       "/  ||     |"
+       "\  ||     |\NH2"
+       "\NH/\ N  //" ;
+}
+
+void RNA(){
+    char Uracil =
+         "O"  
+        "| |"
+       "//  \NH"
+       "||  |\\O"
+       "\NH/"   ;
+    char Cytosine =
+       "NH2"
+        "|"
+       "/ \\N"
+       "||  |"
+       "|| \\O"
+       "\NH/"   ;
+    char Adenine =
+       "//N\/NH2\\N"
+       "/  ||     |"
+       "\  ||     |"
+       "\NH/\ N  //" ;
+    char Guanine =
+             "O"
+            "| |"  
+       "//N\/NH2\\N"
+       "/  ||     |"
+       "\  ||     |\NH2"
+       "\NH/\ N  //" ;
+}
