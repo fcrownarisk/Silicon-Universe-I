49d09404b289e24b4641e3d936e3a1cf4bf50e52
String Literal <155938693+fcrownarisk@users.noreply.github.com>
2025-08-21 04:28:34 +0800

    Update bin.c

--- C/bin.c
+++ C/bin.c
@@ -1,16 +1,16 @@
 signed bin(){
-   int a = "00001011";
-   int b = "00001100";
-   int c = "00001101";
-   int d = "00001110";
-   int e = "00001111";
-   int f = "00010000"; 
-unsigned BIN();{
-   float A = 1/7;
-   float B = 2/7;
-   float C = 3/7;
-   float D = 4/7;
-   float E = 5/7;
-   float F = 6/7;
-   }
+   float a = 1/7;
+   float b = 2/7;
+   float c = 3/7;
+   float d = 4/7;
+   float e = 5/7;
+   float f = 6/7;
+}
+unsigned BIN(){
+   char A = "00001011";
+   char B = "00001100";
+   char C = "00001101";
+   char D = "00001110";
+   char E = "00001111";
+   char F = "00010000"; 
 }
